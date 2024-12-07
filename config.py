10b98985da392bf56c2616b70e36c07d6f2338d4
WanThinnn <thienlai159@gmail.com>
2024-12-07 16:14:41 +0700

    edit config.py

--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 import loguru
-
+import os
 loguru.logger.add("log/DEBUG.log", encoding="utf-8", enqueue=True, backtrace=True, diagnose=True, level="DEBUG")
 loguru.logger.add("log/INFO.log", encoding="utf-8", enqueue=True, backtrace=True, diagnose=True, level="INFO")
 loguru.logger.add("log/ERROR.log", encoding="utf-8", enqueue=True, backtrace=True, diagnose=True, level="ERROR")
 loguru.logger.add("log/WARNING.log", encoding="utf-8", enqueue=True, backtrace=True, diagnose=True, level="WARNING")
 
-SOLC_BIN_PATH = "/home/wanthinnn/Documents/NT521/Project/CrossFuzz/myenv/bin/solc"  # set to your solc path
-
-
+# SOLC_BIN_PATH = "/home/wanthinnn/Documents/NT521/Project/CrossFuzz/myenv/bin/solc"  # set to your solc path
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # Thư mục chứa script Python
+# print(BASE_DIR)
+SOLC_BIN_PATH = os.path.join(BASE_DIR, "myenv/bin/solc")
+# print(SOLC_BIN_PATH)
 ################
 
 

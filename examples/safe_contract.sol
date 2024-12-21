4a256da6e110a6fcc92b7f161064f326507ccb72
Mai Nguyen Nam Phuong - Cyber Security <namphuong.sec.job@gmail.com>
2024-12-21 14:19:34 +0700

    Update safe_contract.sol

--- examples/safe_contract.sol
+++ examples/safe_contract.sol
@@ -1,140 +1,71 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity 0.4.26;
 
-/// @title Sub Contract
+/**
+ * @title SafeMath Library
+ * @dev Math operations with safety checks that throw on error
+ */
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "SafeMath: addition overflow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        require(b <= a, "SafeMath: subtraction overflow");
+        uint256 c = a - b;
+        return c;
+    }
+}
+
 contract Sub {
+    using SafeMath for uint256;
     mapping(address => uint256) private balances;
-    address private immutable owner;
 
-    event BalanceAdded(address indexed user, uint256 amount);
-    event BalanceSubtracted(address indexed user, uint256 amount);
-
-    error NotOwner();
-    error ZeroAddress();
-    error InsufficientBalance();
-
-    modifier onlyOwner() {
-        if (msg.sender != owner) revert NotOwner();
-        _;
+    /**
+     * @dev Add balance for a specific address
+     * @param _addr Address to add balance to
+     * @param _amount Amount to add
+     */
+    function addBalances(address _addr, uint256 _amount) public {
+        require(_addr != address(0), "Invalid address");
+        balances[_addr] = balances[_addr].add(_amount);
     }
 
-    constructor() {
-        owner = msg.sender;
-    }
-
-    function addBalances(address _addr, uint256 _amount) external onlyOwner {
-        if (_addr == address(0)) revert ZeroAddress();
-        balances[_addr] += _amount;
-        emit BalanceAdded(_addr, _amount);
-    }
-
-    function subtractBalances(address _addr, uint256 _amount) external onlyOwner {
-        if (_addr == address(0)) revert ZeroAddress();
-        if (balances[_addr] < _amount) revert InsufficientBalance();
-        balances[_addr] -= _amount;
-        emit BalanceSubtracted(_addr, _amount);
-    }
-
-    function checkBalance(address _addr) external view returns (uint256) {
+    /**
+     * @dev Check balance of a specific address
+     * @param _addr Address to check balance for
+     * @return Balance of the address
+     */
+    function checkBalance(address _addr) public view returns (uint256) {
         return balances[_addr];
     }
 }
 
 contract Child {
     uint256 private inner;
-
-    function getInner() public view returns (uint256) {
-        return inner;
-    }
-
-    function setInner(uint256 _value) internal {
-        inner = _value;
-    }
 }
 
 contract E is Child {
-    Sub public immutable sub;
+    Sub private sub;
     uint256 private count;
     bool private flag;
-    address private immutable owner;
 
-    uint256 private constant MAX_WITHDRAW = 10 ether;
-    uint256 private constant MIN_BALANCE = 1 ether;
-    uint256 private _status;
-
-    event Withdrawal(address indexed user, uint256 amount);
-    event BalanceUpdated(address indexed user, uint256 newBalance);
-    event CountUpdated(uint256 newCount);
-    event FlagUpdated(bool newFlag);
-
-    error NotOwner();
-    error ReentrancyGuard();
-    error ZeroAddress(); 
-    error InvalidAmount();
-    error InsufficientBalance();
-    error CountExceeded();
-    error TransferFailed();
-
-    modifier onlyOwner() {
-        if (msg.sender != owner) revert NotOwner();
-        _;
-    }
-
-    modifier nonReentrant() {
-        if (_status != 0) revert ReentrancyGuard();
-        _status = 1;
-        _;
-        _status = 0;
-    }
-
-    constructor(Sub _sub) {
-        if (address(_sub) == address(0)) revert ZeroAddress();
+    /**
+     * @dev Constructor to set the Sub contract
+     * @param _sub Address of the Sub contract
+     */
+    function E(Sub _sub) public {
+        require(address(_sub) != address(0), "Invalid Sub contract address");
         sub = _sub;
-        owner = msg.sender;
     }
 
-    function setCount(uint256 _count) external onlyOwner {
-        count = _count;
-        emit CountUpdated(_count);
-    }
-
-    function addBalance(address _addr, uint256 _amount) external onlyOwner {
-        if (_addr == address(0)) revert ZeroAddress();
-        if (_amount == 0) revert InvalidAmount();
-        sub.addBalances(_addr, _amount);
-        count += 1;
-        emit CountUpdated(count);
-    }
-
-    function getFlag() external view returns (bool) {
-        return flag;
-    }
-
-    function setFlag(bool _flag) external onlyOwner {
-        flag = _flag;
-        emit FlagUpdated(_flag);
-    }
-
-    function withdraw(address payable _addr, uint256 _amount) external nonReentrant {
-        if (_addr == address(0)) revert ZeroAddress();
-        if (_amount == 0 || _amount > MAX_WITHDRAW) revert InvalidAmount();
-        if (sub.checkBalance(msg.sender) < MIN_BALANCE) revert InsufficientBalance();
-        if (sub.checkBalance(msg.sender) < _amount) revert InsufficientBalance();
-
-        sub.subtractBalances(msg.sender, _amount);
-
-        if (count > 50) revert CountExceeded();
-        count += 2;
-        emit CountUpdated(count);
-
-        (bool sent,) = _addr.call{value: _amount}("");
-        if (!sent) revert TransferFailed();
-
-        emit Withdrawal(msg.sender, _amount);
-        emit BalanceUpdated(msg.sender, sub.checkBalance(msg.sender));
-    }
-
-    receive() external payable {
-        emit BalanceUpdated(msg.sender, address(this).balance);
+    /**
+     * @dev Set a new Sub contract
+     * @param _sub Address of the new Sub contract
+     */
+    function setSub(Sub _sub) public {
+        require(address(_sub) != address(0), "Invalid Sub contract address");
+        sub = _sub;
     }
 }
